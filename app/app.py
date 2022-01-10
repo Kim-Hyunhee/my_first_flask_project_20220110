745360a23cd71664c37c5b36c893369aeb159a26
김현희 <tta_bi@naver.com>
2022-01-10 10:06:08 +0900

    메인 주소로 오면 Hello World 문장 보여주기

--- app/app.py
+++ app/app.py
@@ -6,5 +6,10 @@ def create_app():
     
     # 서버에 대한 세팅 진행
     
+    @app.route("/")   # 만들고 있는 서버의 / (아무것도 붙이지 않은 주소) 로 접속하면 보여줄 내용
+    def test():
+        # return 내용 : HTML 등 웹 프론트엔드 태그 <h1>은 제목으로 제일 큰 글씨로 보여줌
+        return "<h1>Hello World!</h1>"   # Hello World 문장 리턴 => 이 내용을 사용자에게 보여주겠다.
+    
     # 이 서버를 사용하도록 결과로 내보내자
     return app

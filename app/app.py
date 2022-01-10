c04146b2f06011e18323a4a2e153931564d9719c
김현희 <tta_bi@naver.com>
2022-01-10 10:08:13 +0900

    주소 추가 체험

--- app/app.py
+++ app/app.py
@@ -7,9 +7,13 @@ def create_app():
     # 서버에 대한 세팅 진행
     
     @app.route("/")   # 만들고 있는 서버의 / (아무것도 붙이지 않은 주소) 로 접속하면 보여줄 내용
-    def test():
+    def home():
         # return 내용 : HTML 등 웹 프론트엔드 태그 <h1>은 제목으로 제일 큰 글씨로 보여줌
         return "<h1>Hello World!</h1>"   # Hello World 문장 리턴 => 이 내용을 사용자에게 보여주겠다.
+    
+    @app.route("/test") # 서버의 /test 주소로 오면 수행해줄 일을 작성
+    def test():
+        return "이곳은 테스트 페이지 입니다."  # 서버/test 로 오면 보여줄 내용
     
     # 이 서버를 사용하도록 결과로 내보내자
     return app
